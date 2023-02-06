206c2499b123465d262bf2b4e893f4c2cf5cbe17
chris-wamae <chriswamae123@gmail.com>
2023-02-06 04:56:22 +0300

    Completed Assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,36 @@
 # Your code here!
+def my_method param
+    puts "Running my_method"
+    param + 1
+  end
+
+puts my_method(1)
+
+#greet_programmer
+ def greet_programmer 
+    puts "Hello, programmer!"
+ end
+
+ #greet
+ def greet name
+    puts "Hello, #{name}!"
+ end
+
+ #greet_with_default
+ def greet_with_default name="programmer"
+   puts "Hello, #{name}!"
+ end
+
+ #add
+ def add num1 ,num2
+num1 + num2
+ end
+
+ #halve
+ def halve num
+   if num.class != Integer
+      return nil
+   end
+   num/2
+end
+ 
